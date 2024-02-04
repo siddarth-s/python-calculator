ff33f8d805933d80829e7ec33a90c9347a478b15
Siddarth Sathyanarayanan <siddarth7090@gmail.com>
2024-02-03 20:47:07 -0500

    Added python file to add two numbers

---
+++ src/add.py
@@ -0,0 +1,11 @@
+def add(a:int, b:int) -> int:
+    '''
+    This function adds two numbers and returns the result.
+    Parameters:
+        a (int): Number 1
+        b (int): Number 2
+
+    Returns:
+        Sum of a + b which is also an integer
+    '''
+    return a + b
