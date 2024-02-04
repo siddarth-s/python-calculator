c4c3e7318277387a4597a93dcde77eca658caba1
siddarth-s <siddarth7090@gmail.com>
2024-02-03 20:58:28 -0500

    Added subtract, multiply, divide, main files

---
+++ src/divide.py
@@ -0,0 +1,11 @@
+def divide(a:int, b:int) -> int:
+    '''
+    This function divides two numbers and returns the result.
+    Parameters:
+        a (int): Number 1
+        b (int): Number 2
+
+    Returns:
+        a/b
+    '''
+    return a / b
---
+++ src/subtract.py
@@ -0,0 +1,11 @@
+def subtract(a:int, b:int) -> int:
+    '''
+    This function subtracts two numbers and returns the result.
+    Parameters:
+        a (int): Number 1
+        b (int): Number 2
+
+    Returns:
+        Difference of a - b which is also an integer
+    '''
+    return a - b
